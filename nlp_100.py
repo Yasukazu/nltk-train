6dd4b3c824520d5c7de27e529b026e74fab61b70
Makino,Yasukazu <mkn@yskz.dev>
2025-05-09 01:18:41 +0900

    rand_str

--- nlp_100.py
+++ nlp_100.py
@@ -25,3 +25,37 @@ def ciconv(c: str):
     return chr(219 - ord(c[0])) if c[0].islower() else c[0]
 def cipher(src: str):
     return ''.join([ciconv(c) for c in src])
+
+'''09. Typoglycemia
+スペースで区切られた単語列に対して，各単語の先頭と末尾の文字は残し，それ以外の文字の順序をランダムに並び替えるプログラムを作成せよ．
+ただし，長さが４以下の単語は並び替えないこととする．適当な英語の文（例えば
+"I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
+）を与え，その実行結果を確認せよ．
+'''
+import random
+def rand_str(s: str):
+    rr = []
+    random.seed()
+    ss = [c for c in s]
+    while len(rr) < len(s):
+        n = random.randrange(len(s))
+        if (c:=ss[n]) != '\0':
+            rr.append(c)
+            ss[n] = '\0'
+    return ''.join(rr)
+def rand_splits(s: str):
+    ss = s.split()
+    breakpoint()
+    out = []
+    for s in ss:
+        if len(s) <= 4:
+            out.append(s)
+        else:
+            hd = s[0]
+            tl = s[-1]
+            md = s[1:-1]
+            rnd_md = rand_str(md)
+            out.append(hd + rnd_md + tl)
+    return ' '.join(out)
+
+            
